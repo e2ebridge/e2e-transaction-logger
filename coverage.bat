654db34de1374a3e3d8fef17652af97f5eca6085
Cyril Schmitt <cschmitt@e2e.ch>
2014-03-12 15:38:08 +0100

    base

---
+++ coverage.bat
@@ -0,0 +1,1 @@
+node_modules/.bin/istanbul cover test/nodeunittraverse.js -- "test/lib/tests"
---
+++ test/lib/tests/test.test.js
@@ -0,0 +1,13 @@
+/**
+ * Author: cschmitt
+ * Copyright: E2E Technologies Ltd
+ */
+"use strict";
+
+exports.test = function(test) {
+    test.expect(1);
+
+    test.ok(true);
+
+    test.done();
+};
